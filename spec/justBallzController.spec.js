c8ad3d9798f7120315d09e9be46a05c23baa646e
ALRW <awerner1@googlemail.com>
2016-01-19 11:40:41 +0000

    in progress re-establishing angular testing environment

--- spec/justBallzController.spec.js
+++ spec/justBallzController.spec.js
@@ -1,53 +1,40 @@
-describe('JustBallz Controller', function(){
-  describe('justBallzController',function(){
-    beforeEach(module('JustBallz'));
+describe('JustBallzController', function(){
+  beforeEach(module('JustBallz', function($provide){
 
-    var ctrl,scope;
+  }));
 
-    beforeEach(inject(function($controller){
-      scope = {};
-      ctrl = $controller('JustBallzController', {$scope:scope});
-    }));
+  var $controller;
+
+  beforeEach(inject(function(_$controller_){
+    $controller = _$controller_;
+    var scope = {};
+    var _window = {};
+    var ctrl = $controller('JustBallzController', {$scope: scope, $window: window1});
+  }));
+
+
 
     it('starts with the default view shown',function(){
+      console.log($window);
+      console.log($scope);
+      console.log(ctrl);
       expect(scope.viewPane).toBeDefined();
-      expect(scope.isInView(scope.viewPane)).toBeTruthy();
     });
 
-    describe('#connect', function() {
-
-      it('it has a connect function',function(){
-        expect(scope.connect).toBeDefined();
-      });
-
-      it('changes the view variable', function() {
-        scope.connect();
-        expect(scope.viewPane).toEqual(3);
-      });
-
-    });
-
-    describe("#connected",function(){
-
-      it('it has a connected function',function(){
-        expect(scope.connected).toBeDefined();
-      });
-
-      it('changes the view variable', function(){
-        scope.connected();
-        expect(scope.viewPane).toEqual(4);
-      });
-    });
-
-    describe("#disconnect",function(){
-      it('it has a disconnect function', function(){
-        expect(scope.disconnect).toBeDefined();
-      });
-
-      it('changes the view variable when disconnected', function() {
-        scope.disconnect();
-        expect(scope.viewPane).toEqual(1);
-      });
-    });
-  });
+    // describe('#connect', function() {
+    //
+    //
+    //
+    // });
+    //
+    // describe("#connected",function(){
+    //
+    //
+    // });
+    //
+    // describe("#disconnect",function(){
+    //
+    //
+    //
+    // });
 });
