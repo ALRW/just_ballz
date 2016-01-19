40134766a92657882f4f83b59c3b5a8510266bde
ALRW <awerner1@googlemail.com>
2016-01-19 14:58:34 +0000

    added unit tests for a number of small methods in justBallzController

--- spec/justBallzController.spec.js
+++ spec/justBallzController.spec.js
@@ -12,20 +12,41 @@ describe('JustBallzController', function(){
       expect($scope.viewPane).toEqual(1);
     });
 
-    // describe('#connect', function() {
-    //
-    //
-    //
-    // });
-    //
-    // describe("#connected",function(){
-    //
-    //
-    // });
-    //
-    // describe("#disconnect",function(){
-    //
-    //
-    //
-    // });
+    describe('#connect', function() {
+     it('sets the viewPane to 3 and calls for a blue tooth listener', function(){
+       $scope.connect();
+       expect($scope.viewPane).toEqual(3);
+     });
+
+     it('calls the watcher function', function(){
+       spyOn($scope, 'watcher');
+       $scope.connect();
+       expect($scope.watcher).toHaveBeenCalled();
+     });
+
+    });
+
+    describe("#connected",function(){
+      it('sets the viewPane to 4', function(){
+        $scope.connected();
+        expect($scope.viewPane).toEqual(4);
+      });
+    });
+
+    describe("#disconnect",function(){
+      it('sets the viewPane to 1', function(){
+        $scope.disconnect();
+        expect($scope.viewPane).toEqual(1);
+      });
+    });
+
+    describe('#isInView', function(){
+      it('returns true when the given viewPane is displayed', function(){
+        expect($scope.isInView(1)).toBeTruthy();
+      });
+
+      it('returns false when the given viewPane is not displayed', function(){
+        expect($scope.isInView(3)).toBeFalsy();
+      });
+    });
 });
